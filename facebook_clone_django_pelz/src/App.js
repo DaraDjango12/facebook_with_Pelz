18daf5a1bb893551f50f090eec9161ee90041c64
Oluwapelumi <pelumiiadetoye@gmail.com>
2023-04-04 00:51:42 +0100

    working on login page

--- facebook_clone_django_pelz/src/App.js
+++ facebook_clone_django_pelz/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Login from './Pages/Login';
 
 function App() {
   return (
-    <div className="App text-red-600">
-      COLLECT
+    <div>
+      <Login />
     </div>
   );
 }
---
+++ facebook_clone_django_pelz/src/Pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='bg-[#f0f2f5] h-screen'>
+      <div className='flex justify-center'><img className='w-80 h-40' src='https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg' alt=''/></div>
+      
+      <div className='bg-white w-50 h-[400px] mx-14 rounded-md'>
+         <h4 className='font-semibold text-center'>Log in to Facebook</h4>
+      </div>
+
+      {/* #1877f2 */}
+    </div>
+  )
+}
+
+export default Login
