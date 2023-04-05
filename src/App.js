76af2f5e2c8bf7a7471adcc4c252d01e9e5e0167
DaraDjango12 <supertlux@gmail.com>
2023-04-05 09:46:16 -0700

    left bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Register from './Pages/Register';
+// import Register from './Pages/Register';
 import './App.css';
-import Login from './Pages/Login';
-import TopBar from './components/TopBar';
+// import Login from './Pages/Login';
+// import TopBar from './components/TopBar';
+import LeftSideBar from './components/LeftSideBar';
 
 function App() {
   return (
     <div>
-      <TopBar/>
+      <LeftSideBar/>
+      
     </div>
   );
 }
