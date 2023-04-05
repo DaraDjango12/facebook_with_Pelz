6b63d0d029629cbd065483e54216e83466fd3f5c
Oluwapelumi <pelumiiadetoye@gmail.com>
2023-04-05 01:07:55 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import Register from './Pages/Register';
 import './App.css';
-<<<<<<< HEAD
 // import RightSideBar from './components/RightSideBar';
 // import AddPost from './components/AddPost';
 // import Login from './Pages/Login';
-=======
-import Login from './Pages/Login';
-import TopBar from './components/TopBar';
->>>>>>> fdd8cf82a336b2b7ec3bfa02c8b2723369d631ab
+// import TopBar from './components/TopBar';
+
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      {/* <Login /> */}
-      {/* <AddPost /> */}
-      {/* <RightSideBar /> */}
-=======
-      <TopBar/>
->>>>>>> fdd8cf82a336b2b7ec3bfa02c8b2723369d631ab
+      {/* <TopBar/> */}
     </div>
   );
 }
