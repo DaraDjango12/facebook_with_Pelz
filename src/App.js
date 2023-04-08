71f5be592688aac9a32138d6f369be59d6fe9b33
Oluwapelumi <pelumiiadetoye@gmail.com>
2023-04-08 10:18:21 +0100

    worked on the facebookclone's timeline page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-// import RightSideBar from './components/RightSideBar';
-// import AddPost from './components/AddPost';
-// import Login from './Pages/Login';
-// import TopBar from './components/TopBar';
+import Timeline from './components/Timeline';
+// import Profile from './Pages/Profile';
 
 
 function App() {
   return (
     <div>
-      {/* <TopBar/> */}
+      {/* <Profile /> */}
+      <Timeline />
     </div>
   );
 }
